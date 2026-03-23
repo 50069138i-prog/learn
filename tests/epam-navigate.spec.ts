dba4a03ba3a84db4e6545548b1267c7f5113c059
50069138i-prog <50069138i@gmail.com>
2026-03-23 15:17:50 +0100

    Create tests/epam-navigate.spec.ts

---
+++ tests/epam-navigate.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test';
+
+// EPAM navigation test - initial structure
+
+test('EPAM: navigate to homepage', async ({ page }) => {
+  // BODY_PLACEHOLDER
+});
